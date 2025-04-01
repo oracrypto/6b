afa50797dbb6c1ec521765b403428a216a8c9c07
oracrypto <khalidaitahmed000@gmail.com>
2025-04-01 18:13:59 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,38 @@
+function continueAction() {
+    window.location.href = "https://oracrypto.github.io/search";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const lazyImages = document.querySelectorAll("img[data-src]");
+    const imageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                img.src = img.dataset.src;
+                img.removeAttribute("data-src");
+                observer.unobserve(img);
+            }
+        });
+    });
+    lazyImages.forEach(image => imageObserver.observe(image));
+});
+
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+document.addEventListener('selectstart', function(e) {
+    e.preventDefault();
+});
+
+document.addEventListener('copy', function(e) {
+    e.preventDefault();
+    alert('عذراً، النسخ غير مسموح!');
+});
+
+// تأخير 3 ثوانٍ قبل إزالة صفحة التحميل
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('loading-screen').remove(); // إزالة صفحة التحميل نهائياً
+    }, 1000); // 3 ثوانٍ
+};
